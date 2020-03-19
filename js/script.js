5ed10add79faf65e2aab5da6bd2e90b42186e2b9
Emil Bruun Larsen <60384288+Emilovich98@users.noreply.github.com>
2020-03-19 12:32:06 +0100

    Template (menu and footer)

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function showMobileNav() {
+    
+    let showMenu = document.querySelector("#nav-links-mobile");
+
+    if (showMenu.style.width === "100vw") {
+        showMenu.style.width = "0vw";
+    } else {
+        showMenu.style.width = "100vw";
+    }
+    
+    // burger animation
+    const burger = document.querySelector("#burger");
+    
+    burger.classList.toggle("toggle");
+    
+}
